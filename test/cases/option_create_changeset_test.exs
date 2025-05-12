824162d0569aa55a682015e0c537b6b4d2556c85
FedericoAlcantara <federico.alcantara@gmail.com>
2025-05-12 15:55:40 +0200

    Fix mix setup task and infix options (#1)
    - Mix task test.setup renamed to ctx.test.setup, to avoid conflicts with other apps or libraries.
    - Two new options added to ctx_register_schema
        - :infix and :plural_infix to change the names that are generated.
    - Implemented create_changeset and update_changeset.
    
    Co-authored-by: Federico Alcantara <federico.alcantara@wadvanced.com>

---
+++ test/cases/option_create_changeset_test.exs
@@ -0,0 +1,104 @@
+defmodule Aurora.Ctx.Test.Cases.OptionCreateChangesetTest do
+  use Aurora.Ctx.Test.RepoCase
+
+  alias Aurora.Ctx.Test.Support.Inventory.Product
+
+  defmodule Inventory.Product do
+    use Ecto.Schema
+    import Ecto.Changeset
+
+    @type t :: %__MODULE__{
+            reference: String.t() | nil,
+            name: String.t() | nil,
+            description: String.t() | nil,
+            cost: Decimal.t() | nil,
+            list_price: Decimal.t() | nil,
+            deleted: boolean() | nil,
+            inserted_at: DateTime.t() | nil,
+            updated_at: DateTime.t() | nil
+          }
+
+    schema "products" do
+      field(:reference, :string)
+      field(:name, :string)
+      field(:description, :string)
+      field(:cost, :decimal)
+      field(:list_price, :decimal)
+      field(:deleted, :boolean, default: false)
+
+      timestamps()
+    end
+
+    @spec create_changeset(t() | Ecto.Changeset.t(), map()) :: Ecto.Changeset.t()
+    def create_changeset(product, attrs) do
+      product
+      |> cast(attrs, [
+        :reference,
+        :name,
+        :cost
+      ])
+      |> validate_required([:name])
+      |> validate_length(:reference, max: 30)
+      |> validate_number(:cost, greater_than_or_equal_to: 0)
+    end
+
+    @spec changeset(t() | Ecto.Changeset.t(), map()) :: Ecto.Changeset.t()
+    def changeset(product, attrs) do
+      product
+      |> cast(attrs, [
+        :description,
+        :list_price
+      ])
+      |> validate_number(:list_price, greater_than_or_equal_to: 0)
+    end
+  end
+
+  defmodule Inventory do
+    use Aurora.Ctx
+
+    alias Inventory.Product
+
+    @ctx_repo_module Aurora.Ctx.Repo
+
+    ctx_register_schema(Product, create_changeset: :create_changeset)
+  end
+
+  test "Test function creations" do
+    existing_functions =
+      :functions
+      |> __MODULE__.Inventory.__info__()
+      |> Enum.map(&{&1 |> elem(0) |> to_string(), elem(&1, 1)})
+
+    Product
+    |> Aurora.Ctx.implementable_functions()
+    |> Enum.map(&{&1.name, &1.arity})
+    |> Enum.each(&assert(&1 in existing_functions))
+  end
+
+  test "Test create changeset function" do
+    context = __MODULE__.Inventory
+
+    %{
+      "reference" => "item_001",
+      "name" => "Item 001",
+      "description" => "An Item to be the first",
+      "cost" => 11.12
+    }
+    |> context.create_product!()
+    |> tap(&assert(&1.reference == "item_001"))
+    |> tap(&assert(&1.name == "Item 001"))
+    |> tap(&refute(&1.description == "An Item to be the first"))
+    |> tap(&assert(&1.cost == Decimal.new("11.12")))
+    |> context.update_product(%{
+      description: "Item to be the first one",
+      name: "New Item 001",
+      list_price: 40.05
+    })
+    |> tap(&(assert(elem(&1, 0)) == :ok))
+    |> elem(1)
+    |> tap(&assert(&1.reference == "item_001"))
+    |> tap(&assert(&1.description == "Item to be the first one"))
+    |> tap(&refute(&1.name == "New Item 001"))
+    |> tap(&assert(&1.list_price == Decimal.new("40.05")))
+  end
+end
